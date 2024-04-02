b9fbef7b3a40beead74135df67400bdbf207bb90
TomJuri <mail@tomjuri.de>
2024-04-02 11:50:16 +0000

    mission 1 DONE

--- first_mission.py
+++ first_mission.py
@@ -8,24 +8,21 @@ def start(bot: BaseRobot):
     settings.ki = 0.0
     settings.kd = 2.221
 
- #   p 2.11089 ki 0.0 kd 2.221
-  #  kp 4.90436 ki 0.0 kd 2.221 error 96.1543 tol 0.7244668
-
     # drive to izzy
     bot.drivebase.straight(90)
     bot.drivebase.turn(-44.5)
     gyro_straight(bot, 240, 80, 25)
     # pick up izzy
-    bot.right_attach_motor.dc(-100)
+    bot.right_attach_motor.dc(-80)
     wait(350)
     bot.right_attach_motor.dc(0)
     bot.drivebase.straight(90)
     wait(100)
     # turn chicken
-    bot.left_attach_motor.dc(100)
+    bot.left_attach_motor.dc(-100)
     bot.left_motor.dc(75)
     bot.right_motor.dc(75)
-    wait(3800)
+    wait(5800)
     bot.left_attach_motor.dc(0)
     bot.left_motor.dc(0)
     bot.right_motor.dc(0)
@@ -34,10 +31,28 @@ def start(bot: BaseRobot):
     bot.hub.imu.reset_heading(0)
     bot.left_motor.dc(-80)
     bot.right_motor.dc(-85) # right side gets more power because of resistance
-    wait(1250)
+    wait(950)
     bot.left_motor.dc(0)
     bot.right_motor.dc(0)
-    # turn
-    bot.drivebase.turn(-45 - bot.hub.imu.heading())
-    gyro_straight(bot, 300, 80, 25)
-    bot.drivebase.curve(850, 12)
+    # turn and deposit sam
+    bot.drivebase.turn(-45.5 - bot.hub.imu.heading())
+    gyro_straight(bot, 230, 80, 25)
+    bot.hub.imu.reset_heading(0)
+    bot.drivebase.curve(500, 30) # avoid crashing into the boat
+    bot.drivebase.curve(500, -30)
+    bot.drivebase.turn(0 - bot.hub.imu.heading()) # straighten
+    gyro_straight(bot, 410, 80, 25)
+    bot.drivebase.curve(-255, 88) # curve to the carousel
+    bot.left_attach_motor.dc(-80)
+    wait(3350)
+    bot.left_attach_motor.dc(0)
+    # go back to home base and pick up noah
+    bot.left_motor.dc(-80)
+    bot.right_motor.dc(-80)
+    wait(600)
+    bot.left_motor.dc(0)
+    bot.right_motor.dc(0)
+    bot.drivebase.turn(21)
+    gyro_straight(bot, 250, 80, 25)
+    bot.drivebase.curve(380, 50)
+    gyro_straight(bot, 350, 80, 25)
